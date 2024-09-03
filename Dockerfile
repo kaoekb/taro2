ea39d85447609c50e53f7637deffe66193a10e31
kaoekb <kaoekb@gmail.com>
2024-09-04 01:43:24 +0300

    readme

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
 # Используем официальный образ Python как базовый
-FROM python:3.10
+FROM python:3.10-slim
 
-# Устанавливаем рабочую директорию
+# Устанавливаем рабочую директорию внутри контейнера
 WORKDIR /app
 
 # Копируем файл зависимостей в контейнер
 COPY requirements.txt .
 
-# Устанавливаем зависимости
-RUN pip install --no-cache-dir -r requirements.txt
+# Устанавливаем зависимости с помощью pip
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
 
-# Копируем оставшиеся файлы проекта
+# Копируем все остальные файлы в рабочую директорию контейнера
 COPY . .
 
 # Определяем команду для запуска приложения
--- taro2.py
+++ taro2.py
@@ -3,38 +3,60 @@ from telebot import types
 from card_tools import make_layout
 from chatgpt import prediction
 from settings import BOT_MSG_HELLO, BOT_MSG_AWAIT, TELEGRAM_TOKEN, BOT_MSG_ASKME, BOT_MSG_START
+import logging
+import os
+
+log_dir = "./logs"
+os.makedirs(log_dir, exist_ok=True)
+
+# Настройка логирования
+logging.basicConfig(
+    filename=os.path.join(log_dir, 'bot_log.log'),  # Логи сохраняются в файл в папке /app/logs
+    level=logging.INFO,      # Уровень логирования (INFO и выше)
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 sessions = {}
 
 
 def add_session(user_id):
-    sessions[int(user_id)]: list[dict] = make_layout()
+    logging.info(f"Создание сессии для пользователя {user_id}")
+    sessions[int(user_id)] = make_layout()  # исправил на знак равенства
 
 
 def get_session(user_id):
-    return sessions.get(int(user_id), None)
+    session = sessions.get(int(user_id), None)
+    if session:
+        logging.info(f"Сессия для пользователя {user_id} найдена.")
+    else:
+        logging.info(f"Сессия для пользователя {user_id} не найдена.")
+    return session
 
 
 def delete_session(user_id):
     if int(user_id) in sessions.keys():
+        logging.info(f"Удаление сессии для пользователя {user_id}")
         del sessions[int(user_id)]
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    user_id = message.from_user.id
+    logging.info(f"Пользователь {user_id} запустил бота (/start).")
+    
     with open("images/cat.png", 'rb') as f:
         photo = f.read()
 
     msg = bot.send_photo(message.chat.id, photo,
                          caption=BOT_MSG_HELLO.replace("$NAME", message.from_user.first_name))
 
-    # The "Predict" button making
+    # Создаем кнопку "Гадать"
     markup = types.InlineKeyboardMarkup()
     btn_next = types.InlineKeyboardButton('Гадать', callback_data=f"start {message.from_user.id} {message.chat.id}")
     markup.add(btn_next)
 
-    # Binding button to a message
+    # Привязываем кнопку к сообщению
     bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=msg.message_id, reply_markup=markup)
 
 
@@ -47,34 +69,39 @@ def send_cards(chat_id, card_list, index, user_id):
 
         msg = bot.send_photo(chat_id, photo, caption=card['prediction'])
 
-        # The "Next" button making
+        # Создаем кнопку "Далее"
         markup = types.InlineKeyboardMarkup()
         btn_next = types.InlineKeyboardButton('Далее', callback_data=f"{index} {user_id}")
         markup.add(btn_next)
 
-        # Binding button to a message
+        # Привязываем кнопку к сообщению
         bot.edit_message_reply_markup(chat_id=chat_id, message_id=msg.message_id, reply_markup=markup)
+        logging.info(f"Пользователю {user_id} отправлена карта {index + 1}.")
     else:
-
         bot.send_message(chat_id, card_list[0].get('final_prediction'))
         bot.send_message(chat_id, BOT_MSG_START)
 
         delete_session(user_id)
+        logging.info(f"Все карты отправлены пользователю {user_id}. Сессия завершена.")
+
         if card_list[0].get('final_prediction', False):
             del card_list[0]['final_prediction']
 
 
 @bot.message_handler(content_types=["text"])
 def question_handler(message):
-    if sessions.get(message.from_user.id):
+    user_id = message.from_user.id
+    logging.info(f"Получен текст от пользователя {user_id}: {message.text}")
+    
+    if sessions.get(user_id):
         bot.send_message(message.chat.id, BOT_MSG_AWAIT.replace("$NAME", message.from_user.first_name))
 
-        prediction(layout=sessions[message.from_user.id], question=message.text)
+        logging.info(f"Запуск предсказания для пользователя {user_id}.")
+        prediction(layout=sessions[user_id], question=message.text)
 
-        send_cards(chat_id=message.chat.id, card_list=get_session(message.from_user.id),
-                   index=0, user_id=message.from_user.id)
-
+        send_cards(chat_id=message.chat.id, card_list=get_session(user_id), index=0, user_id=user_id)
     else:
+        logging.info(f"Нет активной сессии для пользователя {user_id}.")
         bot.send_message(message.chat.id, BOT_MSG_START)
 
 
@@ -82,20 +109,24 @@ def question_handler(message):
 def callback_handler(call):
     if call.data:
         if call.data.startswith("start"):
+            logging.info(f"Пользователь {call.from_user.id} нажал кнопку 'Гадать'.")
             bot.send_message(call.message.chat.id, BOT_MSG_ASKME)
             call_data = call.data.split(" ")
             add_session(call_data[1])
 
         else:
             call_data = call.data.split(" ")
+            user_id = call_data[1]
 
-            if get_session(call_data[1]):
-                send_cards(call.message.chat.id, get_session(call_data[1]), int(call_data[0]) + 1,
-                           user_id=call_data[1])
+            if get_session(user_id):
+                send_cards(call.message.chat.id, get_session(user_id), int(call_data[0]) + 1, user_id=user_id)
+                logging.info(f"Пользователь {user_id} нажал 'Далее'. Показана следующая карта.")
                 bot.answer_callback_query(call.id)
             else:
+                logging.warning(f"Сессия для пользователя {user_id} не найдена при нажатии кнопки.")
                 bot.send_message(call.message.chat.id, BOT_MSG_START)
 
 
-# run bot
+# Запуск бота
+logging.info("Запуск бота.")
 bot.polling()
